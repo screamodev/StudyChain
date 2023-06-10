7d3bb06793086055774bb613467fc5863ce94734
Arsenii Yurchenko <yurchenko-a@lxuser-ThinkPad-E14-Gen-2>
2023-06-10 12:41:06 +0300

    Update config for deployment to infura

--- crowdfunding/truffle/truffle-config.js
+++ crowdfunding/truffle/truffle-config.js
@@ -1,4 +1,6 @@
+require("dotenv").config({path: "./.env"});
 const HDWalletProvider = require("@truffle/hdwallet-provider");
+const AccountIndex = 0;
 
 module.exports = {
   contracts_build_directory: "../client/src/contracts",
@@ -8,7 +10,13 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
-    }
+    },
+    goerli_infura: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "https://goerli.infura.io/v3/048e36a50b7348088e8224d073e2b995", AccountIndex)
+      },
+      network_id: 5
+    },
   },
 
   compilers: {
